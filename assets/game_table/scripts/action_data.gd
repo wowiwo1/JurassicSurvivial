1998f8c89e9042f051e231f33d3a1633ff6bf408
wowiwo1 <wowiwo1@gmail.com>
2026-03-27 13:44:08 +0900

    게임테이블 업데이트

--- assets/game_table/scripts/action_data.gd
+++ assets/game_table/scripts/action_data.gd
@@ -3,12 +3,11 @@ extends Resource
 class_name ActionData
 
 @export var id: int
+@export var operation: String
 @export var name: String
-@export var type: int
-@export var spawn_season: String
-@export var action_id: String
-@export var risk: int
-@export var hp: int
-@export var atk: int
-@export var def: int
 @export var desc: String
+@export var option_id: String
+@export var result_card: String
+@export var card_min_value: String
+@export var card_max_value: String
+@export var action_id: String
